9f6a713d9e2271599e358d147bc8efcdc20f5b82
tjrwls0826 <tjrwls0826@naver.com>
2024-03-22 22:24:57 +0900

    Update Assignment1_1.c
    fix typo

--- Assignment1_1.c
+++ Assignment1_1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	char* arr1[4] = { "Korea","Seoul","Mapo","152¹øÁö 2/3" };
+	char* arr1[4] = { "Korea","Seoul","Mapo","152ë²ˆì§€ 2/3" };
 	for (int i = 0; i < sizeof(arr1) / 8; i++) {
 		printf("%s\n", arr1[i]);
 	}
 	printf("\n");
-	char* arr2[4] = { "Korea","Seoul","Jongno","152¹øÁö 2/3" };
+	char* arr2[4] = { "Korea","Seoul","Jongno","152ë²ˆì§€ 2/3" };
 	for (int i = 0; i < sizeof(arr2) / 8; i++) {
 		printf("%s\n", arr2[i]);
 	}

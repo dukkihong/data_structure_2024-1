e3ed8aa8ee0f9680c1317d9b710253f4cbbca08c
dukkihong <127312253+dukkihong@users.noreply.github.com>
2024-03-25 20:59:03 +0900

    Add files via upload

---
+++ 2주차수업/Assignment1_6.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+void hanoi(int num, char from, char temp, char to) {
+	if (num == 1) {
+		printf("%c에서 원반 %d를(을) %c로 옮김\n", from, num, to);
+	}
+	else {
+		hanoi(num - 1, from, to, temp);
+		printf("%c에서 원반 %d를(을) %c로 옮김\n", from,num,to);
+		hanoi(num - 1, temp, from, to);
+	}
+}
+
+int main() {
+	hanoi(3, 'A', 'B', 'C');
+	return 0;
+}

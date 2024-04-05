3c092d1fc1ad1b686ddec2f466b5cc2651e5c937
tjrwls0826 <tjrwls0826@naver.com>
2024-04-06 02:00:20 +0900

    과제 갱신

---
+++ 2주차수업/과제2_3차원배열/assignment2_3d.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+	int arr[2][3][4];
+	int num = 0;
+
+	for (int i = 0; i < 2; i++) {// 면
+		for (int y = 0; y < 3; y++) {// 행
+			for (int x = 0; x < 4; x++) {// 열
+				num++;
+				arr[i][y][x] = num;
+				printf("array[%d][%d][%d] = %d\n", i, y, x, arr[i][y][x]);
+			}
+		}
+	}
+	getchar();
+}
